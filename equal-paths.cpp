b5e440949b97e94b52273d24cabc07e837a609c6
Guangda Fei <guangdaf@usc.edu>
2023-03-22 10:18:11 +0000

    all done with tree traversals

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -12,7 +12,38 @@ using namespace std;
 
 bool equalPaths(Node * root)
 {
-    // Add your code below
 
+    //base cases:
+    if (root == nullptr){
+        return true;
+    }
+
+
+    if (equalPaths(root->left) == true && equalPaths(root->right) == true){
+        if (degreeCal(root->left) != degreeCal(root->right) && (degreeCal(root->left) > 1 || degreeCal(root->right) > 1))
+        {
+            return false;
+        }
+        return true;
+    }
+    else{
+        return false;
+    }
+    
 }
+
+int degreeCal(Node * x)
+{
+    if (x == nullptr){
+        return 0;
+    }
+
+    if (degreeCal(x->left) >= degreeCal(x->right)){
+        return degreeCal(x->left)+1;
+    }
+    else{
+        return degreeCal(x->right)+1;
+    }
+}
+
 
